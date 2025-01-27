2e4917d58853ae3a750bf2abc3fa43786dfdabd7
HanyMohamed <hsaad9231@gmail.com>
2025-01-27 22:53:16 +0200

    🎉🎉 First Init+

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@pinia/nuxt", "@vee-validate/nuxt", "@nuxtjs/seo"],
+  modules: ["@pinia/nuxt", "@nuxtjs/seo", "@vee-validate/nuxt"],
   vite: {
     plugins: [tailwindcss()],
   },
+  robots: {},
 });
