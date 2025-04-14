cf3254be961ad358428265d02a9611d546280950
HanyMohamed <hanymohamedsaadeldean@gmail.com>
2025-04-14 14:50:59 +0200

    ♻️refactor: refactor report h1 title

--- components/programs/BountiesInfo.vue
+++ components/programs/BountiesInfo.vue
@@ -12,7 +12,7 @@
     >
       <img src="~/assets/img/report.svg" alt="report" />
       <h1
-        class="mb-10 bg-gradient-to-r from-[#FFFFFF] to-[#71717A] bg-clip-text pt-4 text-2xl font-bold text-transparent md:text-center md:text-4xl"
+        class="mb-10 bg-gradient-to-r from-[#FFFFFF] to-[#71717A] bg-clip-text pt-4 text-center text-2xl font-bold text-transparent md:text-center md:text-4xl"
       >
         No disclosed reports at the moment
       </h1>
