ea658522b2ac2a554ea8bd27dcc2f7b6996d7468
HanyMohamed <hanymohamedsaadeldean@gmail.com>
2025-05-29 20:38:16 +0300

    ♻️refactor: Change Map

--- components/home/MapSection.vue
+++ components/home/MapSection.vue
@@ -4,11 +4,24 @@
       <h1
         class="w-full bg-gradient-to-b bg-clip-text text-center text-3xl font-bold text-white md:text-4xl"
       >
-        From Egypt to the World <br />
-        Advanced Cybersecurity Training Starts Here
+        <span class="text-2xl text-[#BD292E] md:text-3xl">
+          Global Platform
+        </span>
+        <br />
+        Compete With Others and Carry Your Skills Globally
       </h1>
-      <div class="mx-auto mt-20">
-        <img src="~/assets/img/Map.svg" alt="Map" class="mx-auto w-fit" />
+      <div
+        class="mx-auto mt-20 h-full w-full overflow-hidden md:max-h-[400px] md:max-w-[900px]"
+      >
+        <!-- <img src="~/assets/img/Map.svg" alt="Map" class="mx-auto w-fit" /> -->
+        <img
+          src="~/assets/img/map2.svg"
+          alt="Map"
+          loading="lazy"
+          class="mx-auto md:max-h-[400px] md:max-w-[900px]"
+          width="900px"
+          height="400px"
+        />
       </div>
     </div>
   </section>
